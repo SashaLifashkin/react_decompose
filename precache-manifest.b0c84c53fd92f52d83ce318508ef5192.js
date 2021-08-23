e3e739ce07cb57211217fecfbfbcdacc3dbd77cb
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-23 21:34:28 +0300

    Updates

---
+++ precache-manifest.b0c84c53fd92f52d83ce318508ef5192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c844a54836d6d59950437b478f898a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34c55a7d63b0e6352fca",
+    "url": "./static/css/main.5d14edba.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c55a7d63b0e6352fca",
+    "url": "./static/js/main.3ad27357.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
